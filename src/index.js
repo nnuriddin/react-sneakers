600bf1efb419fd62a8a46b16fe4842ab066f1087
nnuriddin <nuriddindoniyorov2@gmail.com>
2022-05-04 16:00:12 +0500

    I create components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
@@ -9,9 +8,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
